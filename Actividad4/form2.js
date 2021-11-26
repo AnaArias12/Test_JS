647e6239e778b6b068aed20e2bd8806d96f23342
Ana <anabelenariasdiaz@gmail.com>
2021-11-26 20:40:52 +0100

    1º prueba

--- Actividad4/form2.js
+++ Actividad4/form2.js
@@ -3,19 +3,42 @@
 
 const RECURSO = "pizzas.json"
 
-function cargarTamaños(); //Cuando se produzca el evento pulsar en tamaños
+function cargarTamaños(){
+    //Cuando se produzca el evento pulsar en tamaños
 
 let xmlHttp = new XMLHttpRequest()  //lo primero que hace es crear el elemento xmlhttp. Se encarga de hacer las peticiones al servidor.
 
-    xmlHttp.open
+    //función de callback
 
-    xmlHttp.send
+    //Esta funcion se va a ejecutar CADA VEZ que haya un cambio en la propiedad
+    //"readyState"
+    xmlHttp.onreadystatechange = function(){        
+            if( this.readyState == 4){
+            if(this.status == 200){//OK
+                procesarRespuesta(this.responseText)
+            } else {
+                alert("[ERROR]")
+            }
+        }
+    }
+
+    xmlHttp.open('GET', URL_DESTINO + RECURSO, true); // utilizamos la función open (le decimos get porque vamos a hacer una petcion, accedemos a una URL y a un recurso concreto)
+    xmlHttp.send("null")//no mandamos nada, estamos haciendo una peticion get.
+    console.log("responseText: "+xmlHttp.responseText);
+  
+ }
 
 
-function cargarIngredientes();
+
+
+ /*function cargarIngredientes(){
+
+
 
     let xmlHttp = new XMLHttpRequest()  
 
     xmlHttp.open
 
     xmlHttp.send
+}*/
+
